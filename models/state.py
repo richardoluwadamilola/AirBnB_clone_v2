37df78e54027e063710da57a5fdb3135ff01542e
richardoluwadamilola <oluwatobyrichard@gmail.com>
2023-01-03 00:37:59 -0800

    added the models of the project including the sql programs

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, Integer
+from sqlalchemy.orm import relationship
+from models import storage
+import os
+from models.city import City
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column('name', String(128), nullable=False)
+    cities = relationship('City', back_populates="state")
+
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            return [city for city in storage.all(City).values()
+                    if city.state_id == self.id]
+
+
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    City.state = relationship('State', back_populates="cities")
