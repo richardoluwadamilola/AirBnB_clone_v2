baa9908249ed682c067a9e4fe4428a227fcd7336
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2023-01-06 08:27:10 +0100

    fix

--- models/city.py
+++ models/city.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-
 import os
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(
