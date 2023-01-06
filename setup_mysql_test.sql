826a4376a22ae6f6cc8aa13a3c1e792c91987b7b
Musa Godwin <musa.godwin8112@gmail.com>
2023-01-06 09:38:59 +0100

    file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,5 @@
--- create the database
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- create new user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- grant all privileges on the database to the user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
--- grant SELECT privilege to the user on the performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
